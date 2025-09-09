8004812e6826b3a2460e9f46c874ba84089a8c2d
GIRIVASAN <133723902+GIRI-79@users.noreply.github.com>
2025-09-09 13:41:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: './', // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
